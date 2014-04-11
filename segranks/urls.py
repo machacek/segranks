0fa7a9d0bdd62d3f5bf6f13e6327d9d094d7c1c9
Matous Machacek <machacekmatous@gmail.com>
2014-04-11 23:19:18 +0200

    urls refactored

--- segranks/urls.py
+++ segranks/urls.py
@@ -8,7 +8,7 @@ from segranks.views import ProjectListView, AnnotateView, AboutView
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.backends.default.urls')),
-    url(r'^projects/$', ProjectListView.as_view(), name="ranks.views.projectlistview"),
-    url(r'^projects/(?P<pk>\d+)/$', AnnotateView.as_view(), name="ranks.views.annotateview"),
+    url(r'^$', ProjectListView.as_view(), name="ranks.views.projectlistview"),
+    url(r'^project-(?P<pk>\d+)/$', AnnotateView.as_view(), name="ranks.views.annotateview"),
     url(r'^about/', AboutView.as_view()),
 )
